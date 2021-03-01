634d5a816febc972d08659fd1a0b46e6dfa71d3b
Roman Nix <rnix@yandex.ru>
2021-03-01 19:09:22 +0700

    Add tests for CRUD for task statuses

--- app/Http/Controllers/TaskStatusController.php
+++ app/Http/Controllers/TaskStatusController.php
@@ -6,8 +6,10 @@ use App\Models\TaskStatus;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
 
 class TaskStatusController extends Controller
 {
@@ -37,11 +39,11 @@ class TaskStatusController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return Response
+     * @return RedirectResponse|Response
      */
     public function store(Request $request)
     {
-        //
+        return $this->validateAndSaveTaskStatus(new TaskStatus(), $request);
     }
 
     /**
@@ -61,9 +63,9 @@ class TaskStatusController extends Controller
      * @param  int  $id
      * @return Response
      */
-    public function edit($id)
+    public function edit(string $id)
     {
-        //
+
     }
 
     /**
@@ -71,21 +73,57 @@ class TaskStatusController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
-     * @return Response
+     * @return RedirectResponse|Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, string $id)
     {
-        //
+        /** @var TaskStatus $taskStatus */
+        $taskStatus = TaskStatus::query()->findOrFail((int) $id);
+        return $this->validateAndSaveTaskStatus($taskStatus, $request);
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return Response
+     * @return RedirectResponse|Response
      */
     public function destroy($id)
     {
-        //
+        $taskStatus = TaskStatus::query()->find((int) $id);
+        if ($taskStatus) {
+            $taskStatus->delete();
+        }
+
+        flash(__('Status has been deleted'))->success();
+
+        /* @phpstan-ignore-next-line */
+        return redirect()
+            ->route('task_statuses.index');
+    }
+
+    private function validateAndSaveTaskStatus(TaskStatus $taskStatus, Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            flash(__('Incorrect name'))->error();
+
+            /* @phpstan-ignore-next-line */
+            return redirect()
+                ->back()
+                ->withInput();
+        }
+
+        $taskStatus->name = $request->get('name');
+        $taskStatus->save();
+
+        flash(__('Status has been saved'))->success();
+
+        /* @phpstan-ignore-next-line */
+        return redirect()
+            ->route('task_statuses.show', ['task_status' => $taskStatus->id]);
     }
 }
--- tests/Feature/TaskStatusControllerTest.php
+++ tests/Feature/TaskStatusControllerTest.php
@@ -25,4 +25,70 @@ class TaskStatusControllerTest extends TestCase
         $response->assertSee('In progress');
         $response->assertSee('Done');
     }
+
+    public function testStore(): void
+    {
+        $data = [
+            'name' => 'Back-log'
+        ];
+
+        $response = $this->post('/task_statuses', $data);
+        $response->assertSessionHasNoErrors();
+        $response->assertRedirect();
+
+        $this->assertDatabaseHas('task_statuses', [
+            'name' => 'Back-log',
+        ]);
+    }
+
+    public function testStoreError(): void
+    {
+        $response = $this->post('/task_statuses', []);
+        $response->assertSessionHas('flash_notification.0.level', 'danger');
+        $response->assertRedirect();
+    }
+
+    public function testUpdate(): void
+    {
+        $taskStatus = TaskStatus::factory()->create([
+            'name' => 'In progress',
+        ]);
+
+        $data = [
+            'name' => 'Fixed name'
+        ];
+
+        $response = $this->put('/task_statuses/' . $taskStatus->id, $data);
+        $response->assertSessionHasNoErrors();
+        $response->assertRedirect();
+
+        $this->assertDatabaseHas('task_statuses', [
+            'id' => $taskStatus->id,
+            'name' => 'Fixed name',
+        ]);
+    }
+
+    public function testDelete(): void
+    {
+        $taskStatus = TaskStatus::factory()->create([
+            'name' => 'To delete',
+        ]);
+
+        $response = $this->delete('/task_statuses/' . $taskStatus->id);
+        $response->assertSessionHasNoErrors();
+        $response->assertRedirect();
+
+        $this->assertDatabaseMissing('task_statuses', [
+            'id' => $taskStatus->id,
+        ]);
+    }
+
+    public function testDeleteDeleted(): void
+    {
+        $id = 999;
+
+        $response = $this->delete('/task_statuses/' . $id);
+        $response->assertSessionHasNoErrors();
+        $response->assertRedirect();
+    }
 }
